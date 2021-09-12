8bcf38216ba318709c6f45b7f2ac65f883f77dc6
andriy-lanyk <LAM1988@ukr.net>
2021-09-12 21:34:17 +0300

    Add localStorage

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,12 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import contactsReduser from "./reduser";
+
+const contactsPersistConfig = {
+  key: "contacts",
+  storage,
+  blacklist: ["filter"],
+};
 
 const store = configureStore({
   reducer: {
-    contacts: contactsReduser,
+    contacts: persistReducer(contactsPersistConfig, contactsReduser),
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(logger),
 });
 
-export { store };
+const persistor = persistStore(store);
+
+export { store, persistor };
